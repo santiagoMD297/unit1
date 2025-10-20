20c8398bffb707daa65bd6c637a19ddedc881335
santiagoMD297 <santiago.marindiaz.297@my.aquinas.me>
2025-10-20 11:25:23 +0000

    tuff

--- Mining.py
+++ Mining.py
@@ -2,122 +2,88 @@ import random
 
 class MiningGrid:
 
-	def __init__(self):
-		self.grid = []
-  
-	def generate_random(self, h, w, min_val, max_val):
-		"""
-		Generates a grid of size width (h * w) filled with random values
-        between min_val and max_val
-        """
-		for row in range(h):
-			row = []
-			for num in range(w):
-				number = random.randint(min_val, max_val)
-				row.append(number)
-			self.grid.append(row)
+    def __init__(self):
+        self.grid = []
+
+    def generate_random(self, rows, cols, min_val, max_val):
+        """Create a grid of random values"""
+        self.grid = []  # reset grid
+        for r in range(rows):
+            new_row = []
+            for c in range(cols):
+                number = random.randint(min_val, max_val)
+                new_row.append(number)
+            self.grid.append(new_row)
+        return self.grid
+
+    def mine_sector(self, location, amount):
+        """Mine minerals from one spot"""
+        x = location[0]
+        y = location[1]
+        current = self.grid[x][y]
+
+        if amount >= current:
+            mined = current
+            self.grid[x][y] = 0
+        else:
+            mined = amount
+            self.grid[x][y] = current - amount
+
+        return mined
+
+    def bulk_mine(self, mine_list):
+        """Mine multiple places"""
+        total = 0
+        for mine in mine_list:
+            x = mine[0]
+            y = mine[1]
+            amount = mine[2]
+            mined_here = self.mine_sector((x, y), amount)
+            total += mined_here
+        return total
+
+    def find_richest_sector(self):
+        """Find the biggest number in the grid"""
+        richest = 0
+        richest_spot = (0, 0)
+
+        for r in range(len(self.grid)):
+            for c in range(len(self.grid[r])):
+                if self.grid[r][c] > richest:
+                    richest = self.grid[r][c]
+                    richest_spot = (r, c)
+
+        return richest_spot
+
+    def find_depleted_sectors(self):
+        """Find all (x,y) spots that are 0"""
+        empty_spots = []
+
+        for r in range(len(self.grid)):
+            for c in range(len(self.grid[r])):
+                if self.grid[r][c] == 0:
+                    empty_spots.append((r, c))
+
+        return empty_spots
+
+    def average_resource_level(self):
+        """Average value of all minerals"""
+        total = 0
+        count = 0
+
+        for r in range(len(self.grid)):
+            for c in range(len(self.grid[r])):
+                total += self.grid[r][c]
+                count += 1
+
+        if count == 0:
+            return 0
+        else:
+            return total / count
 
 
-	def mine_sector(self, location:tuple, amount:int):
-		"""
-		Mines (removes) amount minerals at location. In the event not enough resources
-        are present, they will all be mined and the sector would become 0 (depleted)
-
-        Args:
-        	location: an (x,y) tuple of the location
-            amount: an int representing the amount of minerals to be extracted
-
-        Returns:
-        	int: the amount of minerals successfully mined
-
-        
-        """
-		x = location[0]
-		y = location[1]
-		current = self.grid[x][y]
-		if amount >= current:
-			mined = current
-			self.grid[x][y] = 0
-		else:
-			mined = amount
-			self.grid[x][y] = current - amount
-		return mined
-
-
-
-	def bulk_mine(self, mine_list):
-		"""
-		Mines the asteroid based on the locations and amount given in mine_list:
-
-        Args:
-        	mine_list (tuple): a list of tuples (x,y,a) containing the x, y location and the amount to be mined
-            					ex: [ (3,4,100), (4, 9, 70) ]
-        Returns:
-        	the total amount mined from all locations
-        """
-		total = 0
-		for mine in mine_list:
-			total += self.grid.mine_sector(mine[0:1], mine[2])
-		return total
-
-
-	def find_richest_sector(self):
-		g = self.grid
-		richest = 0
-		location = []
-
-		for h in g:
-			for w in h:
-				if w > richest:
-					richest = w
-		hight = 0
-		for h in g:
-			width = 0
-			for num in h:
-				if num == richest:
-					location.append(hight)
-					location.append(width)
-					return location
-				width += 1
-			hight += 1
-				
-
-
-		
-
-
-
-	def find_depleted_sectors(self):
-		"""
-        Finds all sectors with 0 resources.
-        Returns a list of (row, column) tuples.
-        """
-		locations0 = []
-		g = self.grid
-		h = 0
-		for hight in g:
-			w = 0
-			for width in hight:
-				if width == 0:
-					
-					
-
-
-
-  	
-	def average_resource_level(self):
-		"""
-        Calculates the average resource level of the asteroid.
-        """
-		pass
-
-	def best_region(self):
-		"""
-      	Determines the best region (2x2 sectors) with the highest mineral count
-        
-        Returns:
-        	A list of 4 (x, y) tuples
-        """
-		pass
-mineg = MiningGrid()
-print(mineg.generate_random(5,5,10,100))
+# TEST RUN
+mine = MiningGrid()
+print(mine.generate_random(5, 5, 10, 100))
+print("Richest spot:", mine.find_richest_sector())
+print("Average:", mine.average_resource_level())
